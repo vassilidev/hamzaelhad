e36483a2a19c9807747982aaccc761f36fba1fa5
Vassili Joffroy <vassilidevnet@gmail.com>
2021-01-21 17:44:49 +0100

    Update User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,12 +35,4 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
